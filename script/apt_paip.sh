b60a50aa8f9f9e9f20b589ba23193e2ace3dc17f
zhangenzhi <zhangenzhi657@gmail.com>
2024-06-18 15:32:31 +0900

    adt qdt.

--- script/apt_paip.sh
+++ script/apt_paip.sh
@@ -23,7 +23,8 @@ module load rocm/5.7.0
 srun /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python ./train/apt_paip.py \
         --data_dir=../paip/output_images_and_masks \
         --resolution=512 \
-        --fixed_length=385 \
+        --fixed_length=1024 \
+        --patch_size=8 \
         --epoch=100 \
         --batch_size=4 \
         --savefile=./output_apt
