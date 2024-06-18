911a737a1e1ba3683b2f34804e05226f986e6f17
zhangenzhi <zhangenzhi657@gmail.com>
2024-06-18 14:45:26 +0900

    try get better loss.

--- model/apt.py
+++ model/apt.py
@@ -230,8 +230,10 @@ class APT(nn.Module):
                 nn.GELU(),
                 nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=2, stride=2, padding=0),
                 nn.GELU(),
-                # nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=2, stride=2, padding=0),
-                # nn.GELU(),
+                nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=2, stride=2, padding=0),
+                nn.GELU(),
+                nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=2, stride=2, padding=0),
+                nn.GELU(),
                 SingleConv2DBlock(64, output_dim, 1)
             )
         else:
