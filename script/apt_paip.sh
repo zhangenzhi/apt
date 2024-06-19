54e179da017363fc49c8c247354155f7ff6dce22
zhangenzhi <zhangenzhi657@gmail.com>
2024-06-19 17:12:31 +0900

    apt trans test.

--- script/apt_paip.sh
+++ script/apt_paip.sh
@@ -27,5 +27,5 @@ srun /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python 
         --patch_size=4 \
         --pretrain=False \
         --epoch=50 \
-        --batch_size=2 \
-        --savefile=./apt-1024-4
+        --batch_size=4 \
+        --savefile=./apt-test
