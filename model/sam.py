bf597af9d1f8bbd3bb696b0d7fe2041012746c27
zezzz <zezzz@zezs-mac-mini.lan>
2024-06-20 17:12:32 +0900

    test sam qdt.

--- model/sam.py
+++ model/sam.py
@@ -28,7 +28,7 @@ def _build_sam_vit(
     checkpoint=None,
 ):
     prompt_embed_dim = 256
-    image_size = [32, 32]
+    image_size = [4*32, 4*32]
     vit_patch_size = 4
     # image_size = [patch_size//2, patch_size*fixed_length//2]
     # vit_patch_size = patch_size//2
