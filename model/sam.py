a02c063dce3f9bc9d2863bdfb508f2e3b40ed061
zezzz <zezzz@zezs-mac-mini.lan>
2024-06-21 22:42:33 +0900

    sam qdt no patch emb.

--- model/sam.py
+++ model/sam.py
@@ -28,10 +28,10 @@ def _build_sam_vit(
     checkpoint=None,
 ):
     prompt_embed_dim = 256
-    image_size = [4//2, 4*1024//2]
-    vit_patch_size = 4//2
-    # image_size = [patch_size//2, patch_size*fixed_length//2]
-    # vit_patch_size = patch_size//2
+    # image_size = [4, 4*1024]
+    # vit_patch_size = 4
+    image_size = [patch_size, patch_size*fixed_length]
+    vit_patch_size = patch_size
     # image_embedding_size = image_size // vit_patch_size
     
     image_encoder=ImageEncoderViT(
