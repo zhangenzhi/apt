44664867b37f536139fb2aa7a507e82a768e5131
zezzz <zezzz@zezs-mac-mini.lan>
2024-06-25 01:35:42 +0900

    better seq.

--- train/sam_qdt.py
+++ train/sam_qdt.py
@@ -74,7 +74,7 @@ def main(args):
     
     # Split the dataset into train, validation, and test sets
     data_path = args.data_dir
-    dataset = PAIPQDTDataset(data_path, args.resolution, args.fixed_length, args.patch_size, normalize=True)
+    dataset = PAIPQDTDataset(data_path, args.resolution, args.fixed_length, args.patch_size, normalize=False)
     dataset_size = len(dataset)
     train_size = int(0.7 * dataset_size)
     val_size = (dataset_size - train_size) // 2
