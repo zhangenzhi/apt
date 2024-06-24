d118a109b14a97536c16b784bb696663f49f418f
zezzz <zezzz@zezs-mac-mini.lan>
2024-06-24 21:59:49 +0900

    revise neck conv.

--- model/sam.py
+++ model/sam.py
@@ -31,8 +31,8 @@ def _build_sam_vit(
     # image_size = [4, 4*1024]
     # vit_patch_size = 4
     # image_size = [patch_size, patch_size*fixed_length]
-    image_size = [512, 512]
-    vit_patch_size = 8
+    image_size = [64*64, 64*64]
+    vit_patch_size = 64
     # image_embedding_size = image_size // vit_patch_size
     
     image_encoder=ImageEncoderViT(
