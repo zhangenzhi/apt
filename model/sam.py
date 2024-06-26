024a5ab9ef3036f1218f5eef8b56b8b896946bdf
zhangenzhi <zhangenzhi657@gmail.com>
2024-06-26 15:23:52 +0900

    test large area patch.

--- model/sam.py
+++ model/sam.py
@@ -28,11 +28,11 @@ def _build_sam_vit(
     checkpoint=None,
 ):
     prompt_embed_dim = 256
-    image_size = [patch_size*32, patch_size*32]
-    vit_patch_size = patch_size
+    # image_size = [patch_size*32, patch_size*32]
+    # vit_patch_size = patch_size
     # image_size = [patch_size, patch_size*fixed_length]
-    # image_size = [512, 512]
-    # vit_patch_size = 16
+    image_size = [512, 512]
+    vit_patch_size = 16
     # image_embedding_size = image_size // vit_patch_size
     
     image_encoder=ImageEncoderViT(
