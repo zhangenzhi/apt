b55b6c7478fa2668ff723ccbf46d5d54aafef986
zhangenzhi <zhangenzhi657@gmail.com>
2024-07-10 18:55:23 +0900

    unet ddp node 2.

--- script/unet_miccai.sh
+++ script/unet_miccai.sh
@@ -27,6 +27,6 @@ module load rocm/5.7.0
 srun -N 2 -n 16 --ntasks-per-node 8 /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python ./train/unet_miccai.py \
         --data_dir=../miccai_patches/ \
         --resolution=512 \
-        --epoch=10 \
+        --epoch=20 \
         --batch_size=8 \
         --savefile=./unet_miccai-16-2
