35028159aadde96c59cc98ba73658f583a394e2a
zezzz <zezzz@zezs-mac-mini.lan>
2024-07-14 22:36:05 +0900

    sam-b pz8 ddp1024 ep400.

--- script/sam_miccai.sh
+++ script/sam_miccai.sh
@@ -27,7 +27,7 @@ module load rocm/5.7.0
 srun -N 16 -n 128 --ntasks-per-node 8 /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python ./train/sam_miccai.py \
         --data_dir=../miccai_patches/ \
         --resolution=512 \
-        --lr=1e-3 \
+        --lr=1e-2 \
         --epoch=100 \
         --batch_size=4 \
         --patch_size=8 \
