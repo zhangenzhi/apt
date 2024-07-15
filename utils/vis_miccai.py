24108d3aa27a6533ac7f815e3c1c3a762a228499
zhangenzhi <zhangenzhi657@gmail.com>
2024-07-15 14:14:15 +0900

    visualize miccai.

--- utils/vis_miccai.py
+++ utils/vis_miccai.py
@@ -47,9 +47,8 @@ def main(path, model_weights, resolution, batch_size, patch_size):
     dataset = MICCAIDataset(path, resolution, normalize=False)
     data_loader = DataLoader(dataset, batch_size=batch_size, num_workers=16, shuffle=False)
     dataset_size= len(dataset)
-    # import pdb
-    # pdb.set_trace()
-    
+
+    model.eval()
     for i,batch in enumerate(data_loader):
         images, masks = batch
         images, masks = images.to(device), masks.to(device)  # Move data to GPU
@@ -58,7 +57,15 @@ def main(path, model_weights, resolution, batch_size, patch_size):
         val_score += score
         
         filename = data_loader.dataset.image_filenames[i*batch_size:min((i+1)*batch_size, dataset_size)]
-        save_predicts(outputs, resolution, filename=filename)
+        save_name=f"predict_patches-{resolution}"
+        
+        for i,fp in enumerate(filename):
+            pardir = os.path.dirname(os.path.dirname(fp))
+            save_path = os.path.join(pardir, save_name)
+            os.makedirs(save_path, exist_ok=True)
+            basename = os.path.basename(fp)
+            save_path = os.path.join(save_path,basename)
+            save_image(outputs[i], save_path)
         
     print("Total mean score:{}".format(val_score/len(data_loader)))
 
