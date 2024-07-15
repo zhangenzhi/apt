c11f138d30ee8746997ec359978f00517f7a5e26
zhangenzhi <zhangenzhi657@gmail.com>
2024-07-15 14:16:28 +0900

    visualize miccai.

--- utils/vis_miccai.py
+++ utils/vis_miccai.py
@@ -50,6 +50,9 @@ def main(path, model_weights, resolution, batch_size, patch_size):
 
     model.eval()
     for i,batch in enumerate(data_loader):
+        import pdb
+        pdb.set_trace()
+        
         images, masks = batch
         images, masks = images.to(device), masks.to(device)  # Move data to GPU
         outputs = model(images)
