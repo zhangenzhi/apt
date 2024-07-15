a7147b5f8796404ec27794bb7eeadec5acd7d7e3
zhangenzhi <zhangenzhi657@gmail.com>
2024-07-15 22:29:01 +0900

    test vis.

--- utils/make_slides.py
+++ utils/make_slides.py
@@ -26,7 +26,7 @@ def patches_merge(slide_dir, patches, patch_size, patch_type, resolution):
     print(width, height)
     # import pdb
     # pdb.set_trace()
-    slide = np.zeros((width, height, 1))
+    slide = np.zeros((width, height))
     for i in range(num_patches_width+1):
         for j in range(num_patches_height+1):
             # Define the coordinates of the current patch
