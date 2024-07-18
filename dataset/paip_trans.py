15e47b2a405e309a822f4d1553e2d2846ce89dea
zhangenzhi <zhangenzhi657@gmail.com>
2024-07-18 15:34:28 +0900

    test.

--- dataset/paip_trans.py
+++ dataset/paip_trans.py
@@ -118,6 +118,9 @@ class PAIPTrans(Dataset):
         mask = self.transform(mask)
         qdt_mask = self.transform(qdt_mask)
         
+        import pdb
+        pdb.set_trace
+        
         mask = mask.long()
         mask = F.one_hot(mask, num_classes=2)
         mask = torch.squeeze(mask)
