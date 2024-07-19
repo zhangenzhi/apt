67e6c118da0f45f3a325def84c6b5b59476cc6f8
zhangenzhi <zhangenzhi657@gmail.com>
2024-07-19 19:04:47 +0900

    test.

--- train/sam_trans.py
+++ train/sam_trans.py
@@ -214,8 +214,8 @@ def sub_trans_plot(image, mask, qmasks, qdt_info, fixed_length, bi, epoch, outpu
         deoced_mask_pred = qdt.deserialize(seq=mask_pred, patch_size=8, channel=3)
         true_score = dice_score(mask_true, targets=deoced_mask_pred)
         
-        print(mask_true.shape)
-        print(deoced_mask_pred.shape)
+        print(mask_true.dtype)
+        print(deoced_mask_pred.dtype)
         # Plot and save images
         plt.figure(figsize=(12, 4))
         plt.subplot(1, 3, 1)
