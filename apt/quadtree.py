f2bd6850465cb45f703685818c625185e1e8ec6b
zhangenzhi <zhangenzhi657@gmail.com>
2024-07-19 16:17:49 +0900

    test.

--- apt/quadtree.py
+++ apt/quadtree.py
@@ -130,6 +130,9 @@ class FixedQuadTree:
     
     def deserialize(self, seq, patch_size, channel):
 
+        import pdb
+        pdb.set_trace()
+        
         seq = np.reshape(seq, (self.fixed_length, patch_size, patch_size, channel))
         seq = seq.astype(int)
         mask = np.zeros(shape=self.domain.shape)
