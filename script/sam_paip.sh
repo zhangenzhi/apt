1275c2bea760d35c249d504a07a4be0d5145cad7
zhangenzhi <zhangenzhi657@gmail.com>
2024-07-20 21:17:44 +0900

    test dice score.

--- script/sam_paip.sh
+++ script/sam_paip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A bif146
-#SBATCH -o sam-l-8.o%J
+#SBATCH -o sam-l-16.o%J
 #SBATCH -t 02:00:00
 #SBATCH -N 1
 #SBATCH -p batch
@@ -23,8 +23,7 @@ module load rocm/5.7.0
 srun /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python ./train/sam_paip.py \
         --data_dir=../paip/output_images_and_masks \
         --resolution=512 \
-        --fixed_length=1024 \
-        --patch_size=8 \
+        --patch_size=16 \
         --pretrain=sam-l \
         --epoch=100 \
         --batch_size=2 \
