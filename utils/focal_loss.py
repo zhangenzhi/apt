4de251610ed2b774fc376b715fa7990d67fc57bc
zezzz <zezzz@zezs-mac-mini.lan>
2024-07-24 00:23:07 +0900

    dice qdt loss.

--- utils/focal_loss.py
+++ utils/focal_loss.py
@@ -27,7 +27,7 @@ class DiceQDTLoss(nn.Module):
         self.patch_size = patch_size
         self.soft_max = torch.nn.Softmax(-1)
 
-    def forward(self, inputs, targets, qdt_value, smooth=1):
+    def forward(self, inputs, targets, qdt_value, smooth=1/256):
         
         #comment out if your model contains a sigmoid or equivalent activation layer
         inputs = F.sigmoid(inputs)       
@@ -44,9 +44,10 @@ class DiceQDTLoss(nn.Module):
         pred = torch.reshape(inputs,shape=(fixed_length, -1))
         true = torch.reshape(targets, shape=(fixed_length, -1))
         intersection = torch.sum(pred * true,dim=-1)
-        dice_loss = 1 - torch.div((2.*intersection + smooth),(torch.sum(pred,dim=-1) + torch.sum(true, dim=-1) + smooth)  )
+        dice_loss = 1 - torch.div((2.*intersection + smooth),(torch.sum(pred,dim=-1) + torch.sum(true, dim=-1) + smooth))
+        value = torch.sum(value,dim=0)/batch_size
         # value = self.soft_max(value)
-        weighted_loss = torch.sum(dice_loss)
+        weighted_loss = torch.sum(dice_loss*value)//fixed_length
         
         return weighted_loss
     
