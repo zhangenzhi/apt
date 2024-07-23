2ed87af4d9c56e57ffa57b8b88c20cc11bd870bc
zezzz <zezzz@zezs-mac-mini.lan>
2024-07-24 00:11:44 +0900

    dice qdt loss.

--- utils/focal_loss.py
+++ utils/focal_loss.py
@@ -41,10 +41,10 @@ class DiceQDTLoss(nn.Module):
         targets = torch.flatten(targets[:,1:,:,:])
         value = torch.reshape(qdt_value,shape=(batch_size, fixed_length))
         
-        pred = torch.reshape(inputs,shape=(batch_size, fixed_length, -1))
-        true = torch.reshape(targets, shape=(batch_size, fixed_length, -1))
+        pred = torch.reshape(inputs,shape=(fixed_length, -1))
+        true = torch.reshape(targets, shape=(fixed_length, -1))
         intersection = torch.sum(pred * true,dim=-1)
-        dice_loss = 1 - (2.*intersection + smooth)/(torch.sum(pred,dim=-1) + torch.sum(true,dim=-1) + smooth)  
+        dice_loss = 1 - torch.div((2.*intersection + smooth),(torch.sum(pred,dim=-1) + torch.sum(true, dim=-1) + smooth)  )
         # value = self.soft_max(value)
         weighted_loss = torch.sum(dice_loss)
         
