6e9dc2396502d48da1ce8b8c5fccec0836590e30
zezzz <zezzz@zezs-mac-mini.lan>
2024-07-24 21:46:07 +0900

    test 1k-32.

--- script/sam_paip.sh
+++ script/sam_paip.sh
@@ -23,7 +23,7 @@ module load rocm/5.7.0
 srun /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python ./train/sam_paip.py \
         --data_dir=../paip/output_images_and_masks \
         --resolution=1024 \
-        --patch_size=32 \
+        --patch_size=16 \
         --pretrain=sam-b \
         --epoch=100 \
         --batch_size=4 \
