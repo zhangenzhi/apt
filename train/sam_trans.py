1299d5b1dea3ccb97d305c48dd2727b2adf16cf1
zezzz <zezzz@zezs-mac-mini.lan>
2024-07-25 21:29:14 +0900

    4k paip pz128.

--- train/sam_trans.py
+++ train/sam_trans.py
@@ -99,7 +99,7 @@ def main(args):
     val_set = test_set = Subset(dataset, val_indices)
     # train_set, val_set, test_set = random_split(dataset, [train_size, val_size, test_size])
 
-    train_loader = DataLoader(train_set, batch_size=args.batch_size, num_workers=8, shuffle=True)
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, num_workers=0, shuffle=True)
     val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False)
     test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False)
 
