b1e551061c4cd4b840bf4b7b384c3bbbaed3384d
zezzz <zezzz@zezs-mac-mini.lan>
2024-07-27 18:08:27 +0900

    solved conflicts.

--- script/sam_trans.sh
+++ script/sam_trans.sh
@@ -24,7 +24,7 @@ srun /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python 
         --data_dir=../paip/output_images_and_masks \
         --resolution=1024 \
         --fixed_length=1024 \
-        --patch_size=8 \
+        --patch_size=4 \
         --pretrain=sam-b \
         --epoch=400 \
         --batch_size=1 \
