088f5ec5f541e5887990b3249dc657210c19b41b
zezzz <zezzz@zezs-mac-mini.lan>
2024-08-05 10:07:02 +0900

    miccai +  paip

--- script/sam_miccai_trans.sh
+++ script/sam_miccai_trans.sh
@@ -23,11 +23,11 @@ module load rocm/5.7.0
 
 srun -N 4 -n 32 --ntasks-per-node 8 /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python ./train/sam_miccai_trans.py \
         --data_dir=../miccai_patches/ \
-        --resolution=32768 \
+        --resolution=16384 \
         --fixed_length=8281 \
         --lr=1e-4 \
         --epoch=1000 \
         --batch_size=1 \
         --patch_size=8 \
         --pretrain=sam-b \
-        --savefile=./sam_miccai_trans_r32k_f8k_n4
+        --savefile=./sam_miccai_trans_r16k_f8k_n4
