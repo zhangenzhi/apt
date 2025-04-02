7736c88d42bb3a03480873aedbfba7c5194c36fb
zhangenzhi <zhangenzhi657@gmail.com>
2025-04-02 14:38:16 +0900

    test

--- model/unet.py
+++ model/unet.py
@@ -41,12 +41,12 @@ class Unet(nn.Module):
         self.decode3 = Decoder(256, 256+128, 256)
         self.decode2 = Decoder(256, 128+64, 128)
         self.decode1 = Decoder(128, 16+64, 16)
-        self.decode0 = nn.Sequential(
+        self.conv_last = nn.Conv2d(16, n_class, 1, 2)
+        self.upscale = nn.Sequential(
             nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True),
             # nn.Conv2d(64, 32, kernel_size=3, padding=1, bias=False),
             # nn.Conv2d(32, 64, kernel_size=3, padding=1, bias=False)
         )
-        self.conv_last = nn.Conv2d(16, n_class, 1, 2)
 
     def forward(self, input):
         # input = self.up_first(input)
@@ -60,8 +60,9 @@ class Unet(nn.Module):
         d3 = self.decode3(d4, e3) # 256,64,64
         d2 = self.decode2(d3, e2) # 128,128,128
         d1 = self.decode1(d2, e1) # 64,256,256
-        input = self.decode0(d1)     # 64,512,512
-        out = self.conv_last(input)  # 1,512,512
+        # d0 = self.decode0(d1)     # 64,512,512
+        out = self.conv_last(d1)  # 1,256,256
+        out = self.upscale(out)
         
         import pdb;pdb.set_trace()
         
