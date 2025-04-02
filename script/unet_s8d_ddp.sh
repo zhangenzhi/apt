191fd1c374fe5b69d538922b343443003b98b22a
zhangenzhi <zhangenzhi657@gmail.com>
2025-04-02 20:44:15 +0900

    test unet s8d ddp.

--- script/unet_s8d_ddp.sh
+++ script/unet_s8d_ddp.sh
@@ -20,7 +20,7 @@ module load rocm/5.7.0
 
 # exec
 srun -N 8 -n 64 --ntasks-per-node 8 python ./train/unet_s8d_ddp.py \
-        --data_dir=./lustre/orion/nro108/world-shared/enzhi/Riken_XCT_Simulated_Data/8192x8192_2d_Simulations/Noise_0.05_Blur_2_sparsity_2_NumAng_3600 \
+        --data_dir=/lustre/orion/nro108/world-shared/enzhi/Riken_XCT_Simulated_Data/8192x8192_2d_Simulations/Noise_0.05_Blur_2_sparsity_2_NumAng_3600 \
         --epoch=100 \
         --batch_size=1 \
         --savefile=./unet-s8d-n8
