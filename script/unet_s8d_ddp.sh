b89882719895d711435cd06e40a4ba1a482c80cc
zezzz <zezzz@zezs-mac-mini.lan>
2025-04-03 18:57:01 +0900

    test unet ddp.

--- script/unet_s8d_ddp.sh
+++ script/unet_s8d_ddp.sh
@@ -2,9 +2,8 @@
 #SBATCH -A lrn075
 #SBATCH -o unet_s8d_ddp.o%J
 #SBATCH -t 02:00:00
-#SBATCH -N 8
+#SBATCH -N 32
 #SBATCH -p batch
-#SBATCH -q debug
 #SBATCH --mail-user=zhangsuiyu657@gmail.com
 #SBATCH --mail-type=END
 
@@ -19,9 +18,9 @@ module load gcc/12.2.0
 module load rocm/5.7.0
 
 # exec
-srun -N 8 -n 32 --ntasks-per-node 8 python ./train/unet_s8d_ddp.py \
+srun -N 32 -n 256 --ntasks-per-node 8 python ./train/unet_s8d_ddp.py \
         --data_dir=/lustre/orion/nro108/world-shared/enzhi/Riken_XCT_Simulated_Data/8192x8192_2d_Simulations/Noise_0.05_Blur_2_sparsity_2_NumAng_3600 \
         --epoch=100 \
         --batch_size=1 \
-        --savefile=./unet-s8d-n8
+        --savefile=./unet-s8d-n32
 # 8281
