5450a5edb72a23466ae3e7d2bb4c42579b2e4fff
zezzz <zezzz@zezs-mac-mini.lan>
2025-04-12 21:06:25 +0900

    test real sample.

--- utils/s8d_inf.py
+++ utils/s8d_inf.py
@@ -26,7 +26,9 @@ def main():
     image = np.fromfile(img_name, dtype=np.uint16).reshape([8192, 8192, 1])
     image = (image[:] / 255).astype(np.uint8)
     image = torch.Tensor(image)
-    save_input_as_image(image, "real_img.png")
+    image = (image - image.min()) / (image.max() - image.min()+1e-4)
+    image = image.permute(2,0,1).unsqueeze(0)
+    save_input_as_image(image[0].permute(1,2,0), "real_img.png")
     
     device = torch.device("cuda" if torch.cuda.is_available() else "mps")
     num_class = 5
@@ -36,7 +38,7 @@ def main():
     model.load_state_dict(torch.load(os.path.join("/lustre/orion/nro108/world-shared/enzhi/apt/unet-s8d-n32-dce", "best_score_model.pth")))
     with torch.no_grad():
         pred = model(image)
-        save_pred_as_mask(pred, "pred.png")
+        save_pred_as_mask(pred[0].permute(1,2,0), "pred.png")
         
 if __name__ == "__main__":
     main()
