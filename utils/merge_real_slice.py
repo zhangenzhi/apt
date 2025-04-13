b92be9eab2510617c4e9cf4f87e4e9eb89006e7f
zezzz <zezzz@zezs-mac-mini.lan>
2025-04-13 20:52:03 +0900

    test real merge

--- utils/merge_real_slice.py
+++ utils/merge_real_slice.py
@@ -41,18 +41,18 @@ def main():
     
     pred_slices = []
     image_slices = []
-    for img_name in image_filenames:
+    for idx,img_name in enumerate(image_filenames):
         image = np.fromfile(img_name, dtype=np.uint16).reshape([8192, 8192, 1])
         image = (image[:] / 255).astype(np.uint8)
         image = torch.Tensor(image)
         image = (image - image.min()) / (image.max() - image.min()+1e-4)
         image = image.permute(2,0,1).unsqueeze(0)
-        save_input_as_image(image[0].permute(1,2,0), f"real_img_{img_name}.png")
+        save_input_as_image(image[0].permute(1,2,0), f"real_img_{idx}.png")
         
         with torch.no_grad():
             image = image.to(device=device)
             pred = model(image)
-            save_pred_as_mask(pred[0], f"pred_{img_name}.png")
+            save_pred_as_mask(pred[0], f"pred_{idx}.png")
             
             pred = pred[0].argmax(axis=0).permute(1,2,0)
             image = image[0].permute(1,2,0)
