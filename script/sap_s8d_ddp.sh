5f4d52291d9389bec91f2f9df923eb2e5df2b258
zezzz <zezzz@zezs-mac-mini.lan>
2025-04-20 19:05:03 +0900

    lr scheduler.

--- script/sap_s8d_ddp.sh
+++ script/sap_s8d_ddp.sh
@@ -20,12 +20,11 @@ module load rocm/6.2.0
 # exec
 srun -N 32 -n 256 --ntasks-per-node 8 python ./train/sap_s8d_ddp.py \
         --data_dir=/lustre/orion/nro108/world-shared/enzhi/Riken_XCT_Simulated_Data/8192x8192_2d_Simulations/Noise_0.05_Blur_2_sparsity_2_NumAng_3600 \
-        --epoch=1000 \
+        --epoch=100 \
         --resolution=8192 \
         --fixed_length=10201 \
         --patch_size=8 \
         --pretrain=sam-b \
-        --epoch=1000 \
         --batch_size=1 \
         --savefile=./sap_s8d_n32-deq2
 # 8281
